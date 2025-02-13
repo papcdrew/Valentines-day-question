3da8b8db33363d34fd18eafbabe3394984782b21
papcdrew <andrewdaepo440@gmail.com>
2025-02-13 13:13:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function moveNoButton() {
+    let x = Math.random() * window.innerWidth * 0.8;
+    let y = Math.random() * window.innerHeight * 0.8;
+    document.getElementById('noBtn').style.left = `${x}px`;
+    document.getElementById('noBtn').style.top = `${y}px`;
+}
+
+function showDateDetails() {
+    document.body.innerHTML = `
+        <div style='text-align:center; margin-top:100px;'>
+            <h1> 🎉 See you on our special date! 🎉 </h1>
+            <img src='luanpic2.png' alt='Celebration' style='width:300px;'>
+            <p><strong>Time:</strong> 7:00 PM</p>
+            <p><strong>Date:</strong> February 14, 2025</p>
+            <p><strong>Place:</strong> Romantic Restaurant 🌹</p>
+        </div>
+    `;
+}
